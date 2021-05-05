e0a5777c6f9d49c175a925df7b67076779df226f
Glitch (free-grove-bread) <none>
2021-05-05 00:45:10 +0000

    🌃💗 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -3,9 +3,26 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+
 $(".search-button").click(function(){
   
+  let userInp = $(".search-term").val()
+  var url = `https://api.giphy.com/v1/gifs/search?q=${userInp}&rating=pg&api_key=dc6zaTOxFJmzC1`
 
+
+  fetch(url)
+
+  .then(function(response){
+    return response.json()
+    
+  })
+  .then(function(data){
+    //console.log(data)
+    console.log(data)
+    
+  $(".main").append(`<img src="${data}">`)
+    
+  })
   
 });
 
